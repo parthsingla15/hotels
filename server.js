b3912e37d6646865c1784d713d408b6f17bbc5dc
parth <singlaparth15@gmail.com>
2024-06-18 20:13:54 +0530

    3rd commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const db=require('./db');
 
+require('dotenv').config();
 const menu=require('./models/menu');
 
 const bodyParser=require('body-parser');
@@ -20,6 +21,9 @@ app.use('/person',personroutes);
 const menuroutes=require('./routes/menuroutes') 
 app.use('/menu',menuroutes);
 
-app.listen(3000,()=>{ 
+
+const PORT=process.env.PORT ||3000;
+
+app.listen(PORT,()=>{ 
   console.log('server is active')
 })
